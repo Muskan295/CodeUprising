6b590657c19a86c16d674c04a6d98ea9b8ed4adc
Muskan <muskanmittal249@gmail.com>
2025-11-27 11:28:50 +0530

    added comments

--- CodeChef/Starters_214/ZeroPermutationScore.java
+++ CodeChef/Starters_214/ZeroPermutationScore.java
@@ -11,11 +11,14 @@ class Codechef
 		while(T>0){
 		    T--;
 		    int N=sc.nextInt();
+            //divide '0' pairs arr into half;
 		    int m=N/2;
 		    int ans=0;
+            //if 'N' is even then (m-1)*(m-1); eg: N=4 , m=2, (ans=1*1); during all operations ,total count of zero-pair is 1;
 		    if(N%2==0){
 		        ans=(m-1)*(m-1);
 		    }
+            //if 'N' is odd;
 		    else{
 		        ans=m*(m-1);
 		    }
